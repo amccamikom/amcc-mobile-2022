aa6e766d35e45cc6033a7f6565bed1907f41cedb
RzkyAul30 <ayu.id883@gmail.com>
2023-06-01 18:01:22 +0700

    menambahkan homepage

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -6,62 +6,153 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: backgroundColor1,
-      body: SafeArea(
-        child: Center(
-          child: SingleChildScrollView(
-            child: Column(
-              children: [
-                SizedBox(
-                  height: 40,
-                ),
-                Card(
-                  margin: EdgeInsets.symmetric(horizontal: 15),
-                  elevation: 3,
-                  shadowColor: backgroundColor2,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(15),
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: backgroundColor1,
+        bottomNavigationBar: Container(
+          width: 30,
+          height: 30,
+          color: backgroundColor2,
+        ),
+        body: Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 24,
+            vertical: 14,
+          ),
+          child: Column(
+            children: [
+              //header
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    children: [
+                      Image.asset(
+                        'assets/logo_amibike_white.png',
+                        scale: 10.0,
+                      ),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      Text(
+                        'AMIBIKE',
+                        style: whiteTextStyle.copyWith(
+                            fontSize: 20, fontWeight: semiBold),
+                      ),
+                    ],
                   ),
-                  child: Padding(
-                    padding: EdgeInsets.symmetric(
-                      horizontal: 25,
-                      vertical: 8,
+                  TextButton(
+                    onPressed: () {},
+                    child: Text(
+                      'LOGOUT',
+                      style: whiteTextStyle.copyWith(
+                          fontSize: 20, fontWeight: semiBold),
                     ),
-                    child: Row(
-                      children: [
-                        Column(
-                          // menambahkan ca.start
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              "David",
-                              style: primaryTextStyle.copyWith(
-                                fontSize: 32,
-                                fontWeight: semiBold,
-                              ),
-                            ),
-                            Text(
-                              "12.3456.78",
-                              style: primaryTextStyle.copyWith(
-                                fontSize: 16,
-                                fontWeight: reguler,
-                              ),
-                            ),
-                          ],
+                  )
+                ],
+              ),
+              SizedBox(
+                height: 42,
+              ),
+              //name container
+              Container(
+                height: 86,
+                width: 326,
+                decoration: BoxDecoration(
+                  color: backgroundColor2,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(
+                    vertical: 10,
+                    horizontal: 15,
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'DAVID',
+                        style: primaryTextStyle.copyWith(
+                          fontSize: 20,
+                          fontWeight: semiBold,
                         ),
-                      ],
-                    ),
+                      ),
+                      Text(
+                        '20.00.0000',
+                        style: primaryTextStyle.copyWith(
+                          fontSize: 20,
+                          fontWeight: semiBold,
+                        ),
+                      ),
+                    ],
                   ),
                 ),
-                SizedBox(
-                  height: 30,
-                ),
-              ],
-            ),
+              ),
+              SizedBox(
+                height: 42,
+              ),
+              //wrap
+              Wrap(
+                runAlignment: WrapAlignment.center,
+                spacing: 15,
+                runSpacing: 15,
+                children: [
+                  NewContainer(
+                    image: 'assets/User.png',
+                  ),
+                  NewContainer(
+                    image: 'assets/User.png',
+                  ),
+                  NewContainer(
+                    image: 'assets/User.png',
+                  ),
+                  NewContainer(
+                    image: 'assets/User.png',
+                  ),
+                ],
+              )
+            ],
           ),
         ),
       ),
+    );
+  }
+}
+
+class NewContainer extends StatelessWidget {
+  String image;
+  NewContainer({super.key, required this.image});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          height: 200,
+          width: 150,
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage(image),
+            ),
+            color: backgroundColor2,
+            borderRadius: BorderRadius.circular(10),
+          ),
+        ),
+        Text(
+          'DAVID',
+          style: primaryTextStyle.copyWith(
+            fontSize: 10,
+            fontWeight: semiBold,
+          ),
+        ),
+        Text(
+          '20.00.0000',
+          style: primaryTextStyle.copyWith(
+            fontSize: 20,
+            fontWeight: semiBold,
+          ),
+        ),
+      ],
     );
   }
 }
--- lib/pages/sign_in_pages.dart
+++ lib/pages/sign_in_pages.dart
@@ -7,69 +7,71 @@ class SignInPages extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(25.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Row(
-              children: [
-                Text("Login Account"),
-                SizedBox(
-                  width: 6,
-                ),
-                Image.asset('assets/User.png'),
-              ],
-            ),
-            Text("Hello, Welcome back!"),
-            SizedBox(
-              height: 41,
-            ),
-            Center(
-              child: Image.asset(
-                'assets/logo_amibike_purple.png',
-                width: 166,
-                height: 202,
+    return SafeArea(
+      child: Scaffold(
+        body: Padding(
+          padding: const EdgeInsets.all(25.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                children: [
+                  Text("Login Account"),
+                  SizedBox(
+                    width: 6,
+                  ),
+                  Image.asset('assets/User.png'),
+                ],
               ),
-            ),
-            SizedBox(height: 50),
-            TextField(
-              decoration: InputDecoration(
-                labelText: 'Email',
-                hintText: 'Enter your email',
-                border: OutlineInputBorder(),
-                prefixIcon: Icon(Icons.email),
+              Text("Hello, Welcome back!"),
+              SizedBox(
+                height: 41,
               ),
-            ),
-            SizedBox(height: 16),
-            TextField(
-              decoration: InputDecoration(
-                labelText: 'Password',
-                hintText: 'Enter your password',
-                border: OutlineInputBorder(),
-                prefixIcon: Icon(Icons.lock),
-                suffixIcon: Icon(Icons.remove_red_eye),
-              ),
-              obscureText: true,
-            ),
-            SizedBox(height: 50),
-            Center(
-              child: ElevatedButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => HomePage()),
-                  );
-                },
-                child: Text('Login'.toUpperCase()),
-                style: ElevatedButton.styleFrom(
-                  primary: backgroundColor1, //warna button
-                  minimumSize: Size(200, 40), //size w & h
+              Center(
+                child: Image.asset(
+                  'assets/logo_amibike_purple.png',
+                  width: 166,
+                  height: 202,
                 ),
               ),
-            )
-          ],
+              SizedBox(height: 50),
+              TextField(
+                decoration: InputDecoration(
+                  labelText: 'Email',
+                  hintText: 'Enter your email',
+                  border: OutlineInputBorder(),
+                  prefixIcon: Icon(Icons.email),
+                ),
+              ),
+              SizedBox(height: 16),
+              TextField(
+                decoration: InputDecoration(
+                  labelText: 'Password',
+                  hintText: 'Enter your password',
+                  border: OutlineInputBorder(),
+                  prefixIcon: Icon(Icons.lock),
+                  suffixIcon: Icon(Icons.remove_red_eye),
+                ),
+                obscureText: true,
+              ),
+              SizedBox(height: 50),
+              Center(
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => HomePage()),
+                    );
+                  },
+                  child: Text('Login'.toUpperCase()),
+                  style: ElevatedButton.styleFrom(
+                    primary: backgroundColor1, //warna button
+                    minimumSize: Size(200, 40), //size w & h
+                  ),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
