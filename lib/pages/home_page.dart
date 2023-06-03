ce00a17b33c467e0436e5a910366042bfaa7b6fd
RzkyAul30 <ayu.id883@gmail.com>
2023-06-03 16:14:26 +0700

    fix home page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -6,62 +6,6 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: backgroundColor1,
-      body: SafeArea(
-        child: Center(
-          child: SingleChildScrollView(
-            child: Column(
-              children: [
-                SizedBox(
-                  height: 40,
-                ),
-                Card(
-                  margin: EdgeInsets.symmetric(horizontal: 15),
-                  elevation: 3,
-                  shadowColor: backgroundColor2,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(15),
-                  ),
-                  child: Padding(
-                    padding: EdgeInsets.symmetric(
-                      horizontal: 25,
-                      vertical: 8,
-                    ),
-                    child: Row(
-                      children: [
-                        Column(
-                          // menambahkan ca.start
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              "David",
-                              style: primaryTextStyle.copyWith(
-                                fontSize: 32,
-                                fontWeight: semiBold,
-                              ),
-                            ),
-                            Text(
-                              "12.3456.78",
-                              style: primaryTextStyle.copyWith(
-                                fontSize: 16,
-                                fontWeight: reguler,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-                SizedBox(
-                  height: 30,
-                ),
-              ],
-            ),
-          ),
-        ),
-      ),
-    );
+    return Scaffold();
   }
 }
